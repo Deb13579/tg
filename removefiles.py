f649239ad79ff7700b91c45c879fe62db9fde3b8
Deb13579 <74122863+Deb13579@users.noreply.github.com>
2022-06-25 19:03:19 +0800

    Add files via upload

---
+++ removefiles.py
@@ -0,0 +1,29 @@
+Python 3.10.5 (tags/v3.10.5:f377153, Jun  6 2022, 16:14:13) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+import time
+import os
+import shutil
+import bruh
+Traceback (most recent call last):
+  File "<pyshell#3>", line 1, in <module>
+    import bruh
+ModuleNotFoundError: No module named 'bruh'
+l0l
+Traceback (most recent call last):
+  File "<pyshell#4>", line 1, in <module>
+    l0l
+NameError: name 'l0l' is not defined
+time.time()
+1655982995.6799135
+time.time()
+1655983003.3164098
+os.path.exists(path)
+Traceback (most recent call last):
+  File "<pyshell#7>", line 1, in <module>
+    os.path.exists(path)
+NameError: name 'path' is not defined
+os.path.exists(path)
+Traceback (most recent call last):
+  File "<pyshell#8>", line 1, in <module>
+    os.path.exists(path)
+NameError: name 'path' is not defined
